54c375c2494a012f4aa5f3ab6e9cee699ac968e6
coolguy741 <forest915820@gmail.com>
2020-11-20 13:20:46 -0700

    FBO ping pong start

--- src/components/Three/Geometry/index.js
+++ src/components/Three/Geometry/index.js
@@ -7,11 +7,22 @@ import { mainVert } from "../shaders/mainVert"
 import { mainFrag } from "../shaders/mainFrag"
 
 const Geometry = props => {
+    const mat = useRef()
+
     const width = window.innerWidth
     const height = window.innerHeight
+    let copyData = true
+    let rtIndex = 0
+    let speed = 0
+
+    var diff = new MouseSpeed()
+    diff.init()
 
     const pointer = new THREE.Vector2()
-    let speed = 0
+
+    const texture = useMemo(() => {
+        new THREE.DataTexture(new Float32Array(width * height))
+    }, [])
 
     const uniforms = useMemo(
         () => ({
@@ -25,7 +36,7 @@ const Geometry = props => {
                 value: window.innerWidth / window.innerHeight,
             },
             u_texture: {
-                value: new THREE.DataTexture(new Float32Array(width * height)),
+                value: texture,
             },
             u_n1: {
                 value: 4096,
@@ -43,8 +54,42 @@ const Geometry = props => {
         []
     )
 
-    var diff = new MouseSpeed()
-    diff.init()
+    const createRT = useMemo(() => {
+        return new THREE.WebGLRenderTarget(
+            width,
+            height,
+            Object.assign({
+                minFilter: THREE.NearestFilter,
+                magFilter: THREE.NearestFilter,
+                stencilBuffer: false,
+                depthBuffer: false,
+                depthWrite: false,
+                depthTest: false,
+            })
+        )
+    })
+
+    const rt = [createRT, createRT]
+
+    const updateRT = (renderer, scene, camera) => {
+        const destIndex = rtIndex === 0 ? 1 : 0
+        const old = rt[rtIndex]
+        const dest = rt[destIndex]
+
+        if (mat.current?.uniforms?.texture !== undefined) {
+            mat.current.uniforms.texture.value = copyData
+                ? texture
+                : old.texture
+        }
+
+        const oldMainTarget = renderer.getRenderTarget()
+        renderer.setRenderTarget(dest)
+        renderer.render(scene, camera)
+        renderer.setRenderTarget(oldMainTarget)
+
+        rtIndex = destIndex
+        copyData = false
+    }
 
     const pointerMove = e => {
         pointer.set(e.x / window.innerWidth, 1 - e.y / window.innerHeight)
@@ -62,8 +107,10 @@ const Geometry = props => {
         uniforms.u_bw1.value = props.isDarkMode ? 1.0 : 0.125
         uniforms.u_bw2.value = props.isDarkMode ? 0.125 : 1.0
 
-        uniforms.u_mouse.value.lerp(pointer, 0.2)
+        // uniforms.u_mouse.value.lerp(pointer, 0.2)
         uniforms.u_speed.value = speed
+
+        updateRT(state.gl, state.scene, state.camera)
 
         const diffSpeed =
             Math.max(Math.abs(diff.speedX), Math.abs(diff.speedY)) * 0.05
@@ -78,6 +125,7 @@ const Geometry = props => {
             <mesh onPointerMove={pointerMove}>
                 <planeBufferGeometry args={[width, height, 1, 1]} />
                 <shaderMaterial
+                    ref={mat}
                     uniforms={uniforms}
                     vertexShader={mainVert}
                     fragmentShader={mainFrag}
