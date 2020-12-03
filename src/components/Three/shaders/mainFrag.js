d1a24a75b1bd2edcc384144f48b416dc8dc2ba97
coolguy741 <forest915820@gmail.com>
2020-12-03 03:24:29 -0700

    Acid theme GLSL colour shader refactor

--- src/components/Three/shaders/mainFrag.js
+++ src/components/Three/shaders/mainFrag.js
@@ -28,8 +28,8 @@ float character(int n, vec2 p) {
 	return u_bw2;
 }
 
-float map(float value, float min1, float max1, float min2, float max2) {
-    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+float map(float value, float inMin, float inMax, float outMin, float outMax) {
+	return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
 }
 
 void main() {
@@ -37,7 +37,7 @@ void main() {
     vec2 uv = gl_FragCoord.xy / u_resolution.xy;
 	uv *= u_resolution.xy/u_resolution.xy;
 
-    vec4 col = texture2D(u_noise, uv);
+	vec4 col = texture2D(u_noise, uv);
     uv = gl_FragCoord.xy;
 
     float gray = col.x;
@@ -46,18 +46,31 @@ void main() {
 	if (gray > 0.5) n = u_n2;
 
 	vec2 p = mod(uv/4.0, 2.0) - vec2(1.0);
-	col.xyz = gray*vec3(character(n, p));
-	if (u_bw1 != 0.99) col.xyz = vec3(character(n, p));
+	// col.xyz = gray*vec3(character(n, p));
+	if (u_bw1 != 0.99)  col.xyz = vec3(character(n, p));
+
 
 	// Terminal theme
+
 	if (u_bw1 == 1.0 && col.x == 1.0) col.xyz = vec3(0.204, 0.522, 0.141);
 	// if (u_bw1 == 1.0 && col.x == 0.0) col.xyz = vec3(0.404, 0.622, 0.441);
 
-	// Acid theme
-	// if (u_bw1 == 0.99) col.xyz = 1. - col.xyz;
-	if (u_bw1 == 0.99) col.xyz += vec3(abs(sin(u_time / 2.4)) + gl_FragCoord.x / u_resolution.x, 0., abs(cos(u_time / 2.)) + gl_FragCoord.y / u_resolution.y);
 
-	gl_FragColor = vec4(col.xyz, 1.0);
+	// Acid theme
+
+	if (u_bw1 == 0.99) col.r -= 1.5;
+	if (u_bw1 == 0.99) col.b -= 1.5;
+
+	vec3 normCol = clamp(col.rgb, 0.0, 1.);
+
+	vec3 col1 = mix(vec3(.9, 0., 1.), vec3(1., 0.1, 0.1), gl_FragCoord.x / u_resolution.x);
+	vec3 col2 = mix(vec3(0.4, 0.1, 1.), vec3(1., 0.1, .9), gl_FragCoord.y / u_resolution.y);
+
+	if (u_bw1 == 0.99) normCol += mix(col1, col2, .4);
+
+	// if (u_bw1 == 0.99 && col.x >= 1.0 && col.y >= 1.0 && col.z >= 1.0) col.xyz = vec3(.8, 1., 0.);
+
+	gl_FragColor = vec4(normCol.xyz, 1.0);
 
 }
 `
