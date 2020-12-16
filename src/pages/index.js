e126f40224fad957801b3c469489de45dcdc98b6
coolguy741 <forest915820@gmail.com>
2020-12-15 17:31:59 -0700

    Easter egg fn

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,9 @@ import Layout from "../components/layout"
 import { getGPUTier } from "detect-gpu"
 import SEO from "../components/seo"
 import MainCanvas from "../components/Three/Canvas"
+import { easterEgg } from "../util/easteregg"
 
 const IndexPage = props => {
-    // useEffect(() => {
-    //     let isMounted = true // note this flag denote mount status
-
-    //     ;(async () => {
-    //         await getGPUTier()
-    //     })().then(data => {
-    //         console.log(data)
-    //         // if (isMounted) props.setGpuTier(data)
-    //     })
-    //     console.log(props.gpu)
-
-    //     return () => {
-    //         isMounted = false
-    //     } // use effect cleanup to set flag false, if unmounted
-    // })
-
     useEffect(() => {
         ;(async () => {
             const gpuTier = await getGPUTier()
@@ -30,6 +15,8 @@ const IndexPage = props => {
             props.setGpuTier(gpuTier)
         })()
     }, [])
+
+    window.easterEgg = easterEgg
 
     return (
         <Layout>
