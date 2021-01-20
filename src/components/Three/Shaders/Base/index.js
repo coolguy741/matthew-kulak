eb4b5b51c6f7f3ea4c3935369046066c2f7c9a58
coolguy741 <forest915820@gmail.com>
2021-01-20 02:06:13 -0700

    SDF field on base theme shader

--- src/components/Three/Shaders/Base/index.js
+++ src/components/Three/Shaders/Base/index.js
@@ -12,306 +12,135 @@ void main() {
 `
 
 export const baseFrag = `
+
 precision highp float;
 
 uniform vec2 u_resolution;
-uniform float u_time;
 uniform float u_ratio;
-uniform vec3 u_mouse;
-uniform float u_speed;
-uniform float u_slider;
-varying vec2 v_uv;
+uniform vec2 u_mouse;
+uniform float u_time;
+
 varying vec3 v_position;
 
+float PI = 3.1415926;
 
-#define USE_AA
-//#define SHOW_AA_COLOR_ONLY
-float aaTreshold = 4.;
+float sdSphere( vec3 p, float s ) {
+    return length(p)-s;
+}
 
+float sdBox( vec3 p, vec3 b ){
+    vec3 q = abs(p) - b;
+    return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
 
-mat3 rotx(float a) { mat3 rot; rot[0] = vec3(1.0, 0.0, 0.0); rot[1] = vec3(0.0, cos(a), -sin(a)); rot[2] = vec3(0.0, sin(a), cos(a)); return rot; }
-mat3 roty(float a) { mat3 rot; rot[0] = vec3(cos(a), 0.0, sin(a)); rot[1] = vec3(0.0, 1.0, 0.0); rot[2] = vec3(-sin(a), 0.0, cos(a)); return rot; }
-mat3 rotz(float a) { mat3 rot; rot[0] = vec3(cos(a), -sin(a), 0.0); rot[1] = vec3(sin(a), cos(a), 0.0); rot[2] = vec3(0.0, 0.0, 1.0); return rot; }
-mat2 rotate(float a) { return mat2(cos(a), sin(a), -sin(a), cos(a)); }
-vec4 render(in vec3 rp, in vec3 rd);
-vec3 lightDir = normalize(vec3(1.0, .2, .0));
+float sdOctahedron(vec3 p, float s) {
+  p = abs(p);
+  float m = p.x+p.y+p.z-s;
+  vec3 q;
+       if( 3.0*p.x < m ) q = p.xyz;
+  else if( 3.0*p.y < m ) q = p.yzx;
+  else if( 3.0*p.z < m ) q = p.zxy;
+  else return m*0.57735027;
+    
+  float k = clamp(0.5*(q.z-q.y+s),0.0,s); 
+  return length(vec3(q.x,q.y-s+k,q.z-k)); 
+}
+
+mat4 rotationMatrix(vec3 axis, float angle) {
+  axis = normalize(axis);
+  float s = sin(angle);
+  float c = cos(angle);
+  float oc = 1.0 - c;
+  
+  return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+              oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+              oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+              0.0,                                0.0,                                0.0,                                1.0);
+}
+
+vec3 rotate(vec3 v, vec3 axis, float angle) {
+  mat4 m = rotationMatrix(axis, angle);
+  return (m * vec4(v, 1.0)).xyz;
+}
+
+float smin( float a, float b, float k ) {
+    float h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0 );
+    return mix( b, a, h ) - k*h*(1.0-h);
+}
 
 float rand(vec2 co){
-    return fract(sin(dot(co.xy ,vec2(2.9898,78.233))) * 43758.5453);
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
 
-float rnd(vec2 p)
-{
-    return abs(rand(p));
+float sdf( vec3 p ) {
+    //   float box = smin(sdBox(p1, vec3(.3)), sdSphere(p1, 0.4), .3);
+    //   float sphere = sdSphere(p - vec3(u_mouse * vec2(2., 1.), 0.), 0.2);
+    //   return smin(box, sphere, 0.5);
+    
+    vec3 p1 = rotate(p, vec3(1.), u_time);
+    float oct = sdOctahedron(p1, .02);
+    return oct;
 }
 
-float value (float x, float randx)
+float opRep(vec3 p, vec3 c)
 {
-    float a = min(x/randx, 1.0);
-    float b = min(1.0, (1.0 - x) / (1.0 - randx));
-    return a + (b - 1.0);
+    vec3 q = mod(p+0.5*c,c)-0.5*c;
+    return sdf( q );
 }
 
-float polynoise(vec2 p)
-{
-    vec2 seed = floor(p);
-    vec2 rndv = vec2( rnd(seed.xy), rnd(seed.yx));
-    vec2 pt = fract(p);
-    float bx = value(pt.x, rndv.x);
-    float by = value(pt.y, rndv.y);
-    return min(bx, by) * abs(rnd(seed.xy));
+vec3 calcNormal( vec3 p ) {
+    const float h = 0.0001;
+    const vec2 k = vec2(1,-1);
+    return normalize( k.xyy*sdf( p + k.xyy*h ) + 
+                      k.yyx*sdf( p + k.yyx*h ) + 
+                      k.yxy*sdf( p + k.yxy*h ) + 
+                      k.xxx*sdf( p + k.xxx*h ) );
 }
 
-float T;
-const float PI = 3.14159265;
-mat2 r1; mat2 r2; mat2 r3;
+void main() {
+  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+  uv.x *= u_resolution.x / u_resolution.y;
 
-float polyfbm(vec2 p)
-{
-    vec2 seed = floor(p);
-    float m1 = polynoise(p * r2);
-    m1 += polynoise ( r1 * (vec2(0.5, 0.5) + p));
-    m1 += polynoise ( r3 * (vec2(0.35, 0.415) + p));
-    m1 *= .35;
-    
-    float m2 = polynoise (r3 * (p * 2.4));
-    m1 += m2 * 0.05;
-    return m1;
-}
+  vec2 newUV = uv - vec2(.5, 0.);
 
-float stonepolyfbm(vec2 p)
-{
-    vec2 seed = floor(p);
-    float m1 = polynoise(p * r2);
-    m1 += polynoise ( r1 * (vec2(0.5, 0.5) + p));
-    m1 *= 0.5;
-	return m1;
-}
+  float dist = length(newUV - vec2(.5));
+  vec3 bg = mix(vec3(.9), vec3(.8), dist);
 
-float sdBox( vec3 p, vec3 b )
-{
-  vec3 d = abs(p) - b;
-  return min(max(d.x,max(d.y,d.z)),0.0) +
-         length(max(d,0.0));
-}
+  vec3 ray = normalize(vec3( (uv - vec2(1., .5)), -1.));
+  vec3 camPos = vec3(sin(u_time / 5.), cos(u_time / 5.), (u_time / -2.));
 
-float peakH(in vec3 rp)
-{
-    return smoothstep(0.3, 0.298, rp.y + sin(rp.z * 10.0) * 0.05);
-}
+  vec3 rayPos = camPos;
+  float t = 0.;
 
-const float GROUND = 0.0;
-const float STONES = 1.0;
-float HIT_ID = GROUND;
+  float tMax = 5.;
 
-float map(in vec3 rp)
-{
-    HIT_ID = GROUND;
-    // stones
-    float x = -(stonepolyfbm(rp.xz * 5.4) - 0.4) * 0.15 + sdBox(rp - vec3(0.0, .05, 0.0), vec3(11.0, .02, 11.0));
-    
-    // all the rest
-    rp.y /= clamp((min(1.0 - abs(rp.z), (1.0 - abs(rp.x))) / 0.15), 0.5, 1.0);
-    float l = rp.y - polyfbm(rp.xz * 1.4) * 1.2;
-    
-    float bounds = sdBox(rp, vec3(30.0, 2.5, 30.0));
-    l = max(l, bounds); 
-    x = max(x, bounds);
-    if (x < l) HIT_ID = STONES; 
-    
-    return min(l, x);
-}
+  for (int i=0;i<256;i++) {
+    vec3 pos = camPos + t*ray;
+    float h = opRep(pos, vec3(.15));
+    if (h<.0001 || t>tMax) break;
+    t+=h;
+  }
 
-vec3 grad(in vec3 rp)
-{
-    vec2 off = vec2(0.005, 0.0);
-    vec3 g = vec3(map(rp + off.xyy) - map(rp - off.xyy),
-                  map(rp + off.yxy) - map(rp - off.yxy),
-                  map(rp + off.yyx) - map(rp - off.yyx));
-    return normalize(g);
-}
+  vec3 color = bg;
 
+  vec3 a = vec3(0.5, 0.5, 0.5);		
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(4.0, 4.0, 4.0);
+  vec3 d = vec3(0.00, 0.33, 0.67);
 
-///////////////////////
-///////// AA CODE   ///
-//////////////////////
-const float UNINITIALIZED = 99.0;
-float aaRayDistance = UNINITIALIZED;
-float aaDistance = UNINITIALIZED;
+  if (t<tMax) {
+    vec3 pos = camPos + t*ray;
+    vec3 normal = calcNormal(pos);
+    color = normal;
+    float diff = dot(vec3(1.), normal);
+    color = vec3(diff);
+    color = a + b * cos(2. * PI * (c * pos * sin(u_time / 20.) + d));
+    float fresnel = pow(1. + dot(ray, normal), 1.);
 
-float prevDist = 0.0;
-float oldSgn = 1.0;
-float resolutionScale = 0.0; // fwidth(uv.x)
+    // color = vec3(fresnel);
+    // color = mix(color, bg, fresnel);
+  }
 
-float getAATreshold(float dist)
-{
-    return dist * 0.5 * resolutionScale * aaTreshold;
-}
-
-void traceAA(float dist, in vec3 ro, in vec3 rp)
-{
-    float sgn = sign(dist - prevDist);
-    
-    float travelled = length(ro - rp);
-    if(aaRayDistance == UNINITIALIZED && oldSgn != sgn && sgn > 0.0 && prevDist <= getAATreshold(travelled))
-    {
-        aaRayDistance = travelled;
-        aaDistance = dist;
-    }
-    oldSgn = sgn;
-    prevDist = dist;
-}
-
-void renderAA(inout vec4 color, in vec3 ro, in vec3 rd)
-{
-    if (aaDistance > 0.0 && aaDistance <= getAATreshold(aaRayDistance))
-    {
-        float aa = mix(1.0, 0.0, aaDistance / getAATreshold(aaRayDistance));
-        color.rgb += aa * render(ro + rd * aaRayDistance, rd).rgb * (1.0 - color.a);
-        color.a += aa;
-        color.a = clamp(color.a, 0.0, 1.0);
-    }
-}
-
-
-//////////////////////
-//////////////////////
-
-float ao(in vec3 rp, in vec3 g)
-{
-    float d = 0.4;
-    float occ = 1.0;
-    
-    for (int i = 0; i < 3; ++i)
-    {
-        float fi = float(i * 2 + 1);
-        d = d * fi;
-        occ -= (1.0 - (map(rp + g * d) / d)) * (1.0 / fi);
-    }
-    occ = clamp(occ, 0.0, 1.0);
-    return occ;
-}
-
-
-vec4 render(in vec3 rp, in vec3 rd)
-{
-    vec3 g = grad(rp);
-    vec4 color1 = vec4(.9, .9, .9, .1);
-    vec4 color2 = vec4(1., 1., 1., .1) * 1.4;
-    
-    float peak = peakH(rp);
-    vec4 color = mix(color1, color2, smoothstep(0.1, 0.12, rp.y + stonepolyfbm(rp.xz * 2.0) * 0.1));
-    color += mix(vec4(1.0), vec4(.0), peak);
-    
-    if (HIT_ID == STONES)
-    {
-        color = vec4(0.1, 0.1, 0.1, 0.0) * 1.8;
-    }
-    
-    float d = dot(g, lightDir);
-    d = clamp(d, 0.00, 1.0);
-    d = mix(d, 1.0, 0.2);
-    color *= d;
-    color = mix(color, color * ao(rp, g), 0.7);
-	return color;
-}
-
-
-vec2 _uv;
-
-void trace(in vec3 rp, in vec3 rd, inout vec4 color)
-{
-    
-    bool hit = false;
-    vec3 ro = rp;
-    vec4 bgcolor = vec4(.9, .9, .9, 1.);
-    
-    float closest = 999.0;
-    vec3 closestPoint = vec3(0.0);
-    float dist = 0.0;
-    for (int i = 0; i < 500; ++i)
-    {
-        
-        dist = map(rp);
-        
-#ifdef USE_AA        
-        traceAA(dist, ro, rp);
-#endif
-        
-        if(dist < 0.0)
-        {
-            hit = true;
-            break;
-        }
-        rp += rd * max(dist, 0.01) * 0.2;
-        
-        if(length(ro - rp) > 5.) break;
-    }
-    
-    // some more steps for better accuracy
-    if(hit)
-    {
-        for (int i = 0; i < 8; ++i)
-        {
-            rp += dist * rd * 0.15;
-	        dist = map(rp);
-        }
-    }
-    
-#ifdef USE_AA
-	renderAA(color, ro, rd);
-#endif
-    
-#ifndef SHOW_AA_COLOR_ONLY        
-    if(hit)
-    {
-        color += render(rp, rd) * (1.0 - color.a);
-    }
-    else
-    {
-	    color = mix(color, bgcolor, 1.0 - color.a);
-    }
-#endif
-    color.rgb = pow(color.rgb, vec3(1.0 / 3.2));
-}
-
-
-mat3 lookat(vec3 from, vec3 to)
-{
-    vec3 f = normalize(to - from);
-    vec3 r = normalize(cross(vec3(0.0, 1.0, 0.0), f));
-    vec3 u = normalize(cross(-r, f));
-    return mat3(r, u, f);
-}
-
-void main()
-{
-    T = u_time;
-	vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-    uv -= vec2(0.5);
-    uv.y /= u_resolution.x / u_resolution.y;
-    _uv = uv;
-    
-    r1 = rotate(.1);
-    r2 = rotate(.1);
-    r3 = rotate(.1);
-    // resolutionScale = fwidth(uv.x);
-    
-    vec2 m = vec2(sin(T * 0.1) * 0.5,  -0.1 - 0.2 * (cos(T * 0.2) * 0.5 + 0.5));
-    if(u_mouse.z > 0.0)
-    {
-		m = ((u_mouse.xy / u_resolution.xy) - vec2(0.5));
-    }
-    
-    vec2 im = vec2(12.0, 2.0) * m;
-    vec3 rd = normalize(vec3(uv, 1.0));
-    vec3 rp = vec3(0.0, 1.0, -5.5);
-    vec3 lookTo = vec3(0.0, 0.0, 1.0);
-    //rp = roty(im.x) * rp;
-    rp.y = -im.y * 4.0;
-    rd = lookat(rp, lookTo) * rd;
-    
-    gl_FragColor = vec4(0.0);
-    trace(rp, rd, gl_FragColor);
+  gl_FragColor = vec4(color, 1.);   
 }
 `
